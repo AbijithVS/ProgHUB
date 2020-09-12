0654a6837cb37c90c8544a10407f67234d62c571
AbijithVS <70831645+AbijithVS@users.noreply.github.com>
2020-09-12 19:37:35 +0530

    Add files via upload

---
+++ Area of circle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 10:27:59 2020
+
+@author: user
+"""
+
+r=float(input("Enter the radius:"))
+area=3.142857*r*r
+print("Area of circle with radius",r,"is:",area)
+
---
+++ Reading File Extension.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 10:32:43 2020
+
+@author: user
+"""
+
+a = input("Input the Filename: ")
+s = a.split('.')
+
+if(s[1]=="py"):
+    print("The extension of file is :" + "Python")
+elif(s[1]=="java"):
+     print("The extension of file is :" + "Java")
+elif(s[1]=="js"):
+     print("The extension of file is :" + "Javascript")
+elif(s[1]=="mat"):
+     print("The extension of file is :" + "MatLab")
+elif(s[1]=="txt"):
+     print("The extension of file is :" + "Text")
+else:
+    print("The extension of file is :" + "Other files")
+    
+    
