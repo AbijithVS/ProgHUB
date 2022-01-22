6cca32e63e53b31bc6a72a7e6ad77d7426a1ab9b
AbijithVS <70831645+AbijithVS@users.noreply.github.com>
2022-01-22 20:43:45 +0530

    Add files via upload

---
+++ set opertaions python.py
@@ -0,0 +1,15 @@
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+  
+# union
+print("Union :", E | N)
+  
+# intersection
+print("Intersection :", E & N)
+  
+# difference
+print("Difference :", E - N)
+  
+# symmetric difference
+print("Symmetric difference :", E ^ N)
+
